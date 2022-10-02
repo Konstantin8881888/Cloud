bce64ebce824553600a7d297b6d788bca1dca5c3
Konstantin <kamagoroff@gmail.com>
2022-10-02 14:39:19 +0300

    Текущие изменения.

---
+++ common/src/main/java/com/geekbrains/nio/PathExamples.java
@@ -0,0 +1,31 @@
+package com.geekbrains.nio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+
+public class PathExamples {
+
+    public static void main(String[] args) throws IOException {
+        
+        Path dir = Path.of("common", "..", "common", "..", "common").normalize();
+        System.out.println(dir);
+        System.out.println(Files.size(dir.resolve("1.txt")));
+        System.out.println(dir.toAbsolutePath());
+        Path file1 = dir.resolve("1.txt");
+        String string = Files.readString(file1);
+        System.out.println(string);
+        // InputStream stream = Files.newInputStream(file1);
+        Files.copy(
+                dir.resolve("image.png"),
+                dir.resolve("copy.png"),
+                StandardCopyOption.REPLACE_EXISTING
+        );
+
+        Files.writeString(file1, "I'm Mike!", StandardOpenOption.APPEND);
+
+    }
+}
---
+++ geek-cloud-client/src/main/java/com/geekbrains/sep22/geekcloudclient/CloudMainController.java
@@ -0,0 +1,140 @@
+package com.geekbrains.sep22.geekcloudclient;
+
+import com.geekbrains.DaemonThreadFactory;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.URL;
+import java.util.*;
+
+import static com.geekbrains.Command.*;
+import static com.geekbrains.FileUtils.readFileFromStream;
+
+public class CloudMainController implements Initializable {
+    public ListView<String> clientView;
+    public ListView<String> serverView;
+    private String currentDirectory;
+
+    private DataInputStream dis;
+
+    private DataOutputStream dos;
+
+    private Socket socket;
+
+    private boolean needReadMessages = true;
+
+    private DaemonThreadFactory factory;
+
+    public void downloadFile(ActionEvent actionEvent) throws IOException {
+        String fileName = serverView.getSelectionModel().getSelectedItem();
+        dos.writeUTF(GET_FILE_COMMAND.getSimpleName());
+        dos.writeUTF(fileName);
+        dos.flush();
+    }
+
+    public void sendToServer(ActionEvent actionEvent) {
+        String fileName = clientView.getSelectionModel().getSelectedItem();
+        String filePath = currentDirectory + "/" + fileName;
+        File file = new File(filePath);
+        if (file.isFile()) {
+            try {
+                System.out.println("File: " + fileName + " sent to server");
+                dos.writeUTF(SEND_FILE_COMMAND.getSimpleName());
+                dos.writeUTF(fileName);
+                dos.writeLong(file.length());
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    byte[] bytes = fis.readAllBytes();
+                    dos.write(bytes);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } catch (Exception e) {
+                System.err.println("e = " + e.getMessage());
+            }
+        }
+    }
+
+    private void readMessages() {
+        try {
+            while (needReadMessages) {
+                String command = dis.readUTF();
+                if (SEND_FILE_COMMAND.getSimpleName().equals(command)) {
+                    readFileFromStream(dis, currentDirectory);
+                    Platform.runLater(() -> fillView(clientView, getFiles(currentDirectory)));
+                } else if (GET_FILES_LIST_COMMAND.getSimpleName().equals(command)) {
+                    System.out.println("Received command: " + GET_FILES_LIST_COMMAND.getSimpleName());
+                    List<String> files = new ArrayList<>();
+                    int size = dis.readInt();
+                    for (int i = 0; i < size; i++) {
+                        String file = dis.readUTF();
+                        files.add(file);
+                    }
+                    // concurrent set views values
+                    Platform.runLater(() -> fillView(serverView, files));
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Server off");
+        }
+    }
+
+    private void initNetwork() {
+        try {
+            socket = new Socket("localhost", 8189);
+            dis = new DataInputStream(socket.getInputStream());
+            dos = new DataOutputStream(socket.getOutputStream());
+            factory.getThread(this::readMessages, "cloud-client-read-thread")
+                    .start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        needReadMessages = true;
+        factory = new DaemonThreadFactory();
+        initNetwork();
+        setCurrentDirectory(System.getProperty("user.home"));
+        fillView(clientView, getFiles(currentDirectory));
+        clientView.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                String selected = clientView.getSelectionModel().getSelectedItem();
+                File selectedFile = new File(currentDirectory + "/" + selected);
+                if (selectedFile.isDirectory()) {
+                    setCurrentDirectory(currentDirectory + "/" + selected);
+                }
+            }
+        });
+    }
+
+    private void setCurrentDirectory(String directory) {
+        currentDirectory = directory;
+        fillView(clientView, getFiles(currentDirectory));
+    }
+
+    private void fillView(ListView<String> view, List<String> data) {
+        view.getItems().clear();
+        view.getItems().addAll(data);
+    }
+
+    private List<String> getFiles(String directory) {
+        // file.txt 125 b
+        // dir [DIR]
+        File dir = new File(directory);
+        if (dir.isDirectory()) {
+            String[] list = dir.list();
+            if (list != null) {
+                List<String> files = new ArrayList<>(Arrays.asList(list));
+                files.add(0, "..");
+                return files;
+            }
+        }
+        return List.of();
+    }
+
+}
