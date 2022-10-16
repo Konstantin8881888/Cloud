377f1a081454a19ec58dc7d153a92bddaedccd1a
Konstantin <kamagoroff@gmail.com>
2022-10-16 09:43:06 +0300

    Добавлено переименование файлов.

---
+++ common/src/main/java/com/geekbrains/model/FileRename.java
@@ -0,0 +1,23 @@
+package com.geekbrains.model;
+
+import lombok.Getter;
+
+@Getter
+public class FileRename implements CloudMessage
+{
+
+    private final String newFileName;
+    private final String oldFileName;
+
+    public FileRename(String newFileName, String oldFileName)
+    {
+        this.newFileName = newFileName;
+        this.oldFileName = oldFileName;
+    }
+
+    @Override
+    public MessageType getType()
+    {
+        return MessageType.FILE_RENAME;
+    }
+}
